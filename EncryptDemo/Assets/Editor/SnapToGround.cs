e33c3398016e00211726616f2e911b45a9c23ced
PatHealy <pwh5@pitt.edu>
2020-03-08 03:52:04 -0400

    Initial commit; basic racing game

---
+++ EncryptDemo/Assets/Editor/SnapToGround.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class SnapToGround : MonoBehaviour {
+    [MenuItem("Custom/Snap To Ground %g")]
+    public static void Ground() {
+        foreach (var transform in Selection.transforms) {
+            Vector3 bottomPos = transform.position;
+            foreach(Collider c in transform.gameObject.GetComponentsInChildren<Collider>()) {
+                if (c.bounds.min.y < bottomPos.y) {
+                    bottomPos = c.bounds.min;
+                }
+            }
+
+            var hits = Physics.RaycastAll(bottomPos + Vector3.up, Vector3.down, 10f);
+            float minDistance = float.MaxValue;
+            Vector3 hitPoint = transform.position;
+
+            foreach (var hit in hits) {
+                if (hit.collider.gameObject == transform.gameObject)
+                    continue;
+
+                if (hit.distance < minDistance) {
+                    minDistance = hit.distance;
+                    hitPoint = hit.point;
+                }
+            }
+
+            transform.position = hitPoint - (bottomPos - transform.position);
+        }
+    }
+
+}
+
