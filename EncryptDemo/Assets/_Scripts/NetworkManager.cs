5f58c9544d3c2b096c5d98d5cb766501fa79bbcb
PatHealy <pwh5@pitt.edu>
2020-04-15 09:40:35 -0400

    First successful multi-language encrypted transmission

--- EncryptDemo/Assets/_Scripts/NetworkManager.cs
+++ EncryptDemo/Assets/_Scripts/NetworkManager.cs
@@ -1,28 +1,131 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Numerics;
+
 using UnityEngine;
 using UnityEngine.Networking;
+using System.Security.Cryptography;
+
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
 
 public class NetworkManager : MonoBehaviour
 {
     public GameObject[] parents;
     public GameObject[] children;
 
+    string getPublicKeyURL = "http://127.0.0.1:5000/publicKey";
+    string getKeysURL = "http://127.0.0.1:5000/privateKeys";
     string establishConnectionURL = "http://127.0.0.1:5000/attemptStart";
     string sendParentsURL = "http://127.0.0.1:5000/com";
+    string testEncryptionURL = "http://127.0.0.1:5000/testEncryption";
+
+    string publicServerRSA;
+    
+    RSACryptoServiceProvider clientRSA; //Decrypts based on client private key
+    RSACryptoServiceProvider serverRSA; //Encrypts based on server public key
 
     bool started = false;
 
     public int playerNum;
 
+    public enum EncryptionMode {None, AES, DES, DES3, Blowfish, RSA};
+    public EncryptionMode encryptionMode = EncryptionMode.None;
+
     // Start is called before the first frame update
     void Start()
     {
+        clientRSA = new RSACryptoServiceProvider(2048);
+        var pubKey = clientRSA.ExportParameters(false);
+        //converting the public key into a string representation
+        string pubKeyString;
+        {
+            //we need some buffer
+            var sw = new System.IO.StringWriter();
+            //we need a serializer
+            var xs = new System.Xml.Serialization.XmlSerializer(typeof(RSAParameters));
+            //serialize the key into the stream
+            xs.Serialize(sw, pubKey);
+            //get the string from the stream
+            pubKeyString = sw.ToString();
+        }
         StartCoroutine(EstablishConnectionToHost());
     }
 
+    RSACryptoServiceProvider ImportPublicKey(RSAKeySet keys) {
+        RSAParameters rsaParams = new RSAParameters();
+        rsaParams.Modulus = keys.n;
+        rsaParams.Exponent = keys.e;
+
+        RSACryptoServiceProvider csp = new RSACryptoServiceProvider();
+        csp.ImportParameters(rsaParams);
+        return csp;
+    }
+
+    RSACryptoServiceProvider ImportPrivateKey(RSAKeySetPrivate keys) {
+        RSAParameters rsaParams = new RSAParameters();
+        rsaParams.Modulus = keys.n;
+        rsaParams.Exponent = keys.e;
+        rsaParams.D = keys.d;
+        rsaParams.P = keys.p;
+        rsaParams.Q = keys.q;
+
+        RSACryptoServiceProvider csp = new RSACryptoServiceProvider();
+        csp.ImportParameters(rsaParams);
+        return csp;
+    }
+
     IEnumerator EstablishConnectionToHost() {
-        WWWForm form = new WWWForm();
+        // Get the server public keys
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(getPublicKeyURL)) {
+            yield return webRequest.SendWebRequest();
+            if (webRequest.isNetworkError) {
+                Debug.Log("Connection failed");
+            } else {
+                //serverRSA = ImportPublicKey(webRequest.downloadHandler.text);
+                //serverRSA = ImportPublicKey(JsonUtility.FromJson<RSAKeySet>(webRequest.downloadHandler.text));
+                serverRSA = ImportPrivateKey(JsonUtility.FromJson<RSAKeySetPrivate>(webRequest.downloadHandler.text));
+            }
+        }
+
+        //Encrypt my public keys and send them to server, include set of parent objects
+        string testEncryptString = "Test message";
+        byte[] encryptedTest = serverRSA.Encrypt(Encoding.UTF8.GetBytes(testEncryptString), true);
+        //byte[] decryptedTest = serverRSA.Decrypt(encryptedTest, true);
+        //Debug.Log(Encoding.UTF8.GetString(decryptedTest));
+
+        WWWForm form1 = new WWWForm();
+        EncryptedData d = new EncryptedData();
+        d.data = encryptedTest;
+        form1.AddField("test", JsonUtility.ToJson(d));
+
+        using (UnityWebRequest webRequest = UnityWebRequest.Post(testEncryptionURL, form1)) {
+
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.isNetworkError) {
+                Debug.Log("Connection failed");
+            } else {
+                if (webRequest.downloadHandler.text == "go") {
+                    started = true;
+                }
+                Debug.Log(webRequest.downloadHandler.text);
+            }
+        }
+
+        yield return new WaitForFixedUpdate();
+        Debug.Break();
+
+        //Process response, which includes set of keys for ciphers (i.e. create those ciphers)
+
+
+
+        //Wait for all players to connect
+        /*WWWForm form = new WWWForm();
         form.AddField("PlayerID", playerNum);
 
         while (!started) {
@@ -39,12 +142,16 @@ public class NetworkManager : MonoBehaviour
                     Debug.Log(webRequest.downloadHandler.text);
                 }
             }
-        }
+        }*/
 
     }
 
-    IEnumerator GetChildren() {
-        yield return null;
+    string Encrypt(string plaintext) {
+        return plaintext;
+    }
+
+    string Decrypt(string ciphertext) {
+        return ciphertext;
     }
 
     IEnumerator Exchange() {
@@ -56,7 +163,7 @@ public class NetworkManager : MonoBehaviour
         }
 
         WWWForm form = new WWWForm();
-        form.AddField("data", JsonUtility.ToJson(toSend));
+        form.AddField("data", Encrypt(JsonUtility.ToJson(toSend)));
 
         using (UnityWebRequest webRequest = UnityWebRequest.Post(sendParentsURL, form)) {
             yield return webRequest.SendWebRequest();
@@ -64,13 +171,13 @@ public class NetworkManager : MonoBehaviour
             if (webRequest.isNetworkError) {
                 Debug.Log("Connection failed");
             } else {
-                ObjectInfoSet received = JsonUtility.FromJson<ObjectInfoSet>(webRequest.downloadHandler.text);
+                ObjectInfoSet received = JsonUtility.FromJson<ObjectInfoSet>(Decrypt(webRequest.downloadHandler.text));
                 foreach (ObjectInfo o in received.data) {
                     foreach (GameObject g in children) {
                         if (g.name == o.name) {
                             Debug.Log("Set " + g.name);
                             g.transform.position = o.position;
-                            g.transform.rotation = Quaternion.Euler(o.rotation);
+                            g.transform.rotation = UnityEngine.Quaternion.Euler(o.rotation);
                         }
                     }
                 }
@@ -87,5 +194,27 @@ public class NetworkManager : MonoBehaviour
 
     private class ObjectInfoSet {
         public List<ObjectInfo> data;
+    }
+
+    private class KeySet {
+        string AES_key;
+    }
+
+    private class RSAKeySet {
+        public byte[] n;
+        public byte[] e;
+    }
+
+    private class RSAKeySetPrivate {
+        public byte[] n;
+        public byte[] e;
+        public byte[] d;
+        public byte[] p;
+        public byte[] q;
+        public byte[] u;
+    }
+
+    private class EncryptedData {
+        public byte[] data;
     }
 }
--- Server/app.py
+++ Server/app.py
@@ -1,5 +1,16 @@
 from flask import Flask, request, session, url_for, redirect, abort, g, flash, _app_ctx_stack
 import json
+from base64 import b64encode
+# PyCrypto package
+from Crypto.Cipher import AES, DES, DES3, Blowfish, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+#from Crypto.Util.Padding import pad, unpad
+
+from math import log
+def bytes_needed(n):
+	if n == 0:
+		return 1
+	return int(log(n, 256)) + 1
 
 app = Flask(__name__)
 
@@ -7,9 +18,81 @@ game_started = False
 clients = []
 game_data = {}
 
-@app.route('/')
-def home_page():
-	return 'hello'
+public_key = "AAAAAAA"
+
+#Possible methods: None, AES, DES, DES3, Blowfish, RSA, DSA
+method = "None"
+
+# Generate RSA key-pair
+RSA_key_pair = RSA.generate(2048)
+RSA_key_data = {}
+RSA_key_data['n'] = list(RSA_key_pair.n.to_bytes(bytes_needed(RSA_key_pair.n), 'big'))
+RSA_key_data['e'] = list(RSA_key_pair.e.to_bytes(bytes_needed(RSA_key_pair.e), 'big'))
+
+RSA_key_data['d'] = list(RSA_key_pair.d.to_bytes(bytes_needed(RSA_key_pair.d), 'big'))
+RSA_key_data['p'] = list(RSA_key_pair.p.to_bytes(bytes_needed(RSA_key_pair.p), 'big'))
+RSA_key_data['q'] = list(RSA_key_pair.q.to_bytes(bytes_needed(RSA_key_pair.q), 'big'))
+RSA_key_data['u'] = list(RSA_key_pair.u.to_bytes(bytes_needed(RSA_key_pair.u), 'big'))
+
+RSA_decrypter = PKCS1_OAEP.new(RSA_key_pair)
+
+message = b64encode(b'Test message')
+#cipher = PKCS1_OAEP.new(RSA_key_pair)
+ciphertext = RSA_decrypter.encrypt(message)
+#print(ciphertext)
+#plain = cipher.decrypt(ciphertext)
+
+#print(message)
+#print(ciphertext)
+#print(plain)
+
+#key_AES = b'AAAAAAAAAAAAAAAA'
+#aes_cipher = AES.new(key_AES, AES.MODE_CBC) #automatically generates iv
+#aes_iv = b64encode(aes_cipher.iv).decode('utf-8')
+
+def gege_encrypt(plaintext):
+	pass
+
+def gege_decrypt(ciphertext):
+	pass
+
+def encrypt(plaintext):
+	if method == "AES":
+		ct_bytes = aes_cipher.encrypt(pad(plaintext, AES.block_size))
+		ct = b64encode(ct_bytes).decode('utf-8')
+		return ct
+	elif method == "DES":
+		return plaintext
+	elif method == "DES3":
+		return plaintext
+	elif method == "Blowfish":
+		return plaintext
+	elif method == "RSA":
+		return RSA.encrypt(plaintext)
+	else:
+		return plaintext
+
+def decrypt(ciphertext):
+	if method == "AES":
+		return ciphertext
+	elif method == "DES":
+		return ciphertext
+	elif method == "DES3":
+		return ciphertext
+	elif method == "Blowfish":
+		return ciphertext
+	elif method == "RSA":
+		return ciphertext
+	else:
+		return ciphertext
+
+@app.route('/publicKey')
+def get_public_key():
+	return json.dumps(RSA_key_data)
+
+@app.route('/privateKeys', methods=['POST'])
+def get_private_keys():
+	pass
 
 @app.route('/attemptStart', methods=['POST'])
 def establish_connection():
@@ -21,7 +104,18 @@ def establish_connection():
 
 @app.route('/com', methods=['POST'])
 def main_communicate():
-	data = json.loads(request.form['data'])['data']
-	for entry in data:
-		game_data[entry['name']] = entry
-	return json.dumps({'data':list(game_data.values())})
+	data = json.loads(decrypt(request.form['data']))['data']
+	if authenticate(data):
+		for entry in data:
+			game_data[entry['name']] = entry
+	return encrypt(json.dumps({'data':list(game_data.values())}))
+
+@app.route('/testEncryption', methods=['POST'])
+def test_encryption():
+	data = bytes(json.loads(request.form['test'])['data'])
+	print(data)
+	print(RSA_decrypter.decrypt(data))
+
+@app.route('/getExample')
+def get_example():
+	return ciphertext
